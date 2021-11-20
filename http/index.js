e907495ac1ab36203f0e65e1dbc42686a3b2310b
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2021-11-20 18:27:08 +0530

    Commited

--- http/index.js
+++ http/index.js
@@ -4,16 +4,28 @@ const http = require('http');
 
 // req = incoming request
 // res = what re we sending back
+
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('Welcome to The Page');
+    // console.log(req.url);
+    if (req.url == "/") {
+        res.end("Hello Bhaiya Its Working Hello Aliens from Home Page");
     }
-    if (req.url === '/about') {
-        res.end('HEre is Op setup')
+    else if (req.url == "/about") {
+        res.end("Hello Bhaiya Its Working Hello Aliens from About page");
     }
-    res.end(
-        `<h1> OOPS ! </h1>`
-    )
-})
-server.listen(5000)
+    else if (req.url === "/contact") {
+        res.end("Hello Bhaiya Its Working Hello Aliens from contact page");
+    } else{
+        res.writeHead(404,{"Content-type": "text/html"})
+        res.end(" <h1> Error:404 Page Not Found </h1>");
+    }
+});
+server.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});
+
+
+
+
+
 
