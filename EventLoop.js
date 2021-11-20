3a15bb2d88fd41815b48d6931e57e2c9065f4958
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2021-11-20 17:52:28 +0530

    Deleted Some Folders.....

---
+++ EventLoop.js
@@ -0,0 +1,16 @@
+console.log('Vaibhav');
+
+let a = 10
+let i = 0
+
+const task2 = (() => {
+    while(i < a){
+    console.log(i);
+    i++
+    }
+})
+
+console.log(task2);
+
+
+console.log('Pawar');
